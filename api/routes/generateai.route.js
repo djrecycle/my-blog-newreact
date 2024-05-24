725e8ce913fcfa1d3e37150e14213c1bd4dd6275
djrecycle <119471998+djrecycle@users.noreply.github.com>
2024-05-24 21:41:22 +0700

    generateai update

--- api/routes/generateai.route.js
+++ api/routes/generateai.route.js
@@ -5,6 +5,6 @@ import { generateAiContent } from "../controllers/generateai.controller.js";
 const router = express.Router();
 
 // Menggunakan GET karena asumsi hanya mengambil data
-router.get("/ai/content", verifyToken, generateAiContent);
+router.post("/ai/content", verifyToken, generateAiContent);
 
 export default router;
