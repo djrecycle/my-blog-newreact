1cff11743875f2d51fe6c79b6623fd94f3e379f8
djrecycle <119471998+djrecycle@users.noreply.github.com>
2024-05-25 22:27:05 +0700

    /

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://myblognewreactapi-djrecycles-projects.vercel.app/",
+        target: "https://myblognewreactapi-djrecycles-projects.vercel.app",
 
         // "http://localhost:3001",
         // "https://my-blog-react-yeio.vercel.app"
